214e42b2bd8b54ae80411fbaa3d98b6a99ce9438
hyemin12 <khnm31@naver.com>
2022-04-10 15:39:55 +0900

    '반응형만들기'

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function handleCheckbox(e) {
 checkboxs.forEach((checkbox) => {
   checkbox.addEventListener("click", handleCheckbox);
 });
+
+const toggleBtn = document.querySelector(".mobile");
+const nav = document.querySelector(".nav");
+function handleToggle() {
+  if (toggleBtn.classList.contains("close")) {
+    toggleBtn.classList.remove("close");
+    toggleBtn.innerHTML = '<i class="fas fa-bars"></i>';
+    nav.style.left = "-200px";
+  } else {
+    toggleBtn.classList.add("close");
+    toggleBtn.innerHTML = '<i class="fas fa-times"></i>';
+    nav.style.left = "0";
+  }
+}
+toggleBtn.addEventListener("click", handleToggle);
